15337537a7b8eb01e547a08381565c3e52a5e275
Lucas-Lopes-Alves <lucas.lopes7708@gmail.com>
2026-06-08 09:58:52 -0300

    created frontend folder with "npm create vite@latest"

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,9 @@
 import express from "express"
+import "dotenv/config"
 
 const app = express()
-
-const porta = 3000
-
-app.length()
+const porta = process.env.PORT
+const teste = process.env.PIADA
 
 app.listen(porta, ()=>{
     console.log(`Servidor rodando na porta ${porta}`)
